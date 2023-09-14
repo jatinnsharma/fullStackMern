30be07658b5b720ba9f4c134723acae1be850695
jatin <jatinsharma2231@gmail.com>
2023-09-14 12:38:30 +0530

    added-serach-functionality

--- project/reactjs/ecommerce/src/component/header/Header.jsx
+++ project/reactjs/ecommerce/src/component/header/Header.jsx
@@ -4,7 +4,10 @@ const Header = () => {
       <div className="flex bg-blue-600 h-18 pt-3 font-medium  ">
           <div className="text-white text-2xl italic ml-32 ">Flipkart</div>
           <div>
+
               <input className=" mx-5 w-30 h-9 pl-3 rounded-sm " type="text" placeholder="search for products,brands and more"></input>
+
+              
           </div>
           <div className="flex gap-8 text-white ">
               <button className="bg-white text-blue-500 w-36 h-9 rounded-sm">Login</button>
--- project/reactjs/ecommerce/src/component/product/ProductsList.jsx
+++ project/reactjs/ecommerce/src/component/product/ProductsList.jsx
@@ -3,16 +3,25 @@ import React ,{useEffect, useState} from 'react'
 import ProductCard from './ProductCard'
 const ProductsList =  () => {
     const [productData,setProductData] = useState([]);
-     
+    const [productFliterData,setProductFilterData] = useState([])
     useEffect(()=>{
              fetch('https://fakestoreapi.com/products')
                 .then(res=>res.json())
-                .then((completeData)=>{setProductData(completeData)})
+                .then((completeData)=>{
+                    setProductData(completeData);
+                    setProductFilterData(completeData);
+                })
                 .catch((err)=>console.log(err))
     })
+
+    const  handerFliter =(value)=>{
+        const result = productFliterData.filter(e=>e.title.toLowerCase().includes(value))
+        setProductData(result)
+    }
   return (
         <>
         <div className="text-4xl m-10">Best of our products</div>
+        <input type="text" placeholder='Search here...' onChange={e=>handerFliter(e.target.value)}/>
          <div className="grid grid-cols-4">
         {
             productData.map((item,index)=>{
