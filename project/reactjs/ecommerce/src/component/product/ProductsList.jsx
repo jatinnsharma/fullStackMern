1a05f5ffa19fba20355ee0e6d3a439cae3a6e934
jatin <jatinsharma2231@gmail.com>
2023-09-22 00:36:32 +0530

    added-searchBar

--- project/reactjs/ecommerce/src/component/product/ProductsList.jsx
+++ project/reactjs/ecommerce/src/component/product/ProductsList.jsx
@@ -3,28 +3,38 @@ import React ,{useEffect, useState} from 'react'
 import ProductCard from './ProductCard'
 const ProductsList =  () => {
     const [productData,setProductData] = useState([]);
-    const [productFliterData,setProductFilterData] = useState([])
+    // const [productFliterData,setProductFilterData] = useState([])
+
+    // easy search bar 
+    const [searchTerm,setSearchTerm ] = useState("");
+
     useEffect(()=>{
              fetch('https://fakestoreapi.com/products')
                 .then(res=>res.json())
                 .then((completeData)=>{
                     setProductData(completeData);
-                    setProductFilterData(completeData);
+                    // setProductFilterData(completeData);
                 })
                 .catch((err)=>console.log(err))
     })
 
-    const  handerFliter =(value)=>{
-        const result = productFliterData.filter(e=>e.title.toLowerCase().includes(value))
-        setProductData(result)
-    }
+    // const  handerFliter =(value)=>{
+    //     const result = productFliterData.filter(e=>e.title.toLowerCase().includes(value))
+    //     setProductData(result)
+    // }
   return (
         <>
         <div className="text-4xl m-10">Best of our products</div>
-        <input type="text" placeholder='Search here...' onChange={e=>handerFliter(e.target.value)}/>
+        <input className='w-2/6 ml-10 p-2 border-black' type="text" placeholder='Search here...' onChange={e=>setSearchTerm(e.target.value)}/>
          <div className="grid grid-cols-4">
         {
-            productData.map((item,index)=>{
+            productData.filter((val)=>{
+                if(searchTerm === " "){
+                    return val;
+                }else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                    return val;
+                }
+            }).map((item,index)=>{
                 return <ProductCard data={item} key={index}/>
             })
         }
--- project/reactjs/ecommerce/tailwind.config.js
+++ project/reactjs/ecommerce/tailwind.config.js
@@ -12,4 +12,7 @@ module.exports = {
     },
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  }
 }
