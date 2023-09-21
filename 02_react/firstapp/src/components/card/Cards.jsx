646bf1cccb62a1921eece6f17d2f152df90289c9
jatin <jatinsharma2231@gmail.com>
2023-09-22 00:18:49 +0530

    added-searchBar

--- 02_react/firstapp/src/components/card/Cards.jsx
+++ 02_react/firstapp/src/components/card/Cards.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {data} from '../data/data'
 import Card from './Card'
 
 const Cards = () => {
+  const [searchTerm,setSearchTerm] = useState("")
   return (
     <div>
         <h1>Food App</h1>
+        <input style={{width:"40%",height:"40px"}} type='text' placeholder='Serach here ...' onChange={(e)=>{setSearchTerm(e.target.value)}}/>
         <div style={{display:'flex',flexWrap:'wrap'}}>
         {  
-            data.map((items)=>{
+            data.filter((val)=>{
+              if(searchTerm === " "){
+                return val;
+              }else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                return val;
+              }
+            }).map((items)=>{
                 return (
                     <Card data={items}/>
                 )
