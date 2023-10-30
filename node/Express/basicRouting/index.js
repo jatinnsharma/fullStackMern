68e276f97b6739340d5e35c044f422f2e3672d63
jatin <jatinsharma2231@gmail.com>
2023-10-30 22:06:00 +0530

    basic-routing

---
+++ node/Express/basicRouting/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+require('dotenv').config()
+
+const loginPage = path.join(__dirname+'/index.html')
+const header = path.join(__dirname,'header')
+const PORT = process.env.PORT || 8080
+
+app.get('',(req,res)=>{
+res.sendFile(`${header}/Header.html`)
+})
+app.get('/about',(req,res)=>{
+    res.send('<h1>this is a about page</h1>')
+    res.status(200).send()
+})
+ app.get('/login',(req,res)=>{
+   res.sendFile(loginPage)
+ })
+
+ app.listen(PORT,()=>{
+    console.log(`Server is running on PORT ${PORT}`)
+ });
