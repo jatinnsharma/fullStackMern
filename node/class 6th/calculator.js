1c9484381d2fb50b0af12236bb29b04b2ee227d7
jatin <jatinsharma2231@gmail.com>
2023-10-31 12:12:37 +0530

    written by zia os module

---
+++ node/class 6th/calculator.js
@@ -0,0 +1,14 @@
+exports.add=(a,b)=>{
+    return a+b;
+}
+
+exports.div=(a,b)=>{
+    return a/b;
+}
+exports.mul=(a,b)=>{
+    return a*b;
+}
+
+exports.sub=(a,b)=>{
+    return a-b;
+}
--- node/class 6th/index.js
+++ node/class 6th/index.js
@@ -1,19 +1,19 @@
 const addNSub = require('./addNSub.js')
 
 
-// console.log(addNSub.add(4,4));
+console.log(addNSub.add(4,4));
 // console.log(addNSub.sub(5,6))
 
 
 const os = require('os')
 
-console.log(os.homedir()) 
-console.log(os.platform())
-console.log(os.arch())
-console.log(os.cpus())
-console.log(os.hostname())
-console.log(os.totalmem())
-console.log(os.networkInterfaces())
+// console.log(os.homedir()) 
+// console.log(os.platform())
+// console.log(os.arch())
+// console.log(os.cpus())
+// console.log(os.hostname())
+// console.log(os.totalmem())
+// console.log(os.networkInterfaces())
 
 
 
